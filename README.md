07b8ffdcc8a623e3351181be8418006bad181151
gaozhengxin <zhengxingao@live.cn>
2024-10-05 03:41:54 +0800

    update config script

--- README.md
+++ README.md
@@ -1,31 +1,9 @@
 ### Deploy
-ETH
 ```
-forge script script/DeployValueRouter.s.sol --libraries src/lib/Bytes.sol/Bytes:Bytes:0x064B37a5E001E73020166c32B06Eb07372659029 src/lib/CCTPMessage.sol/CCTPMessage:0x6ec82CDE6a64B8752017C0160E272f77BbBeD089 src/lib/SwapMessage:SwapMessageCodec:0xdb38EB076d423557F24dE1634358aa750Fd9A0DB --rpc-url $RPC_URL --private-key $PRIVATE_KEY --broadcast
-```
-
-Avalanche-C
-```
-forge script script/DeployValueRouter.s.sol --libraries src/lib/Bytes.sol/Bytes:Bytes:0xd1BD7F4aCD9490b13fa4401FDE5Ad9fdF478E30A src/lib/CCTPMessage.sol/CCTPMessage:0xa89B7b20bdbfD2D40AE78254f4Eb170e4d93D890 src/lib/SwapMessage:SwapMessageCodec:0x26BB84002D2256BBbD5C283B5c775376c892d2c6 --rpc-url $RPC_URL --private-key $PRIVATE_KEY --broadcast
+forge script script/DeployValueRouter.s.sol --rpc-url $RPC_URL --private-key $PRIVATE_KEY --broadcast
 ```
 
-Optimism
+### Config
 ```
-forge script script/DeployValueRouter.s.sol --libraries src/lib/Bytes.sol/Bytes:Bytes:0x064B37a5E001E73020166c32B06Eb07372659029 src/lib/CCTPMessage.sol/CCTPMessage:0x6ec82CDE6a64B8752017C0160E272f77BbBeD089 src/lib/SwapMessage:SwapMessageCodec:0xdb38EB076d423557F24dE1634358aa750Fd9A0DB --rpc-url $RPC_URL --private-key $PRIVATE_KEY --broadcast
+forge script script/ConfigValueRouter.s.sol  --rpc-url $RPC_URL --private-key $PRIVATE_KEY --broadcast
 ```
-
-Arbitrum
-```
-forge script script/DeployValueRouter.s.sol --libraries src/lib/Bytes.sol/Bytes:Bytes:0x064B37a5E001E73020166c32B06Eb07372659029 src/lib/CCTPMessage.sol/CCTPMessage:0x6ec82CDE6a64B8752017C0160E272f77BbBeD089 src/lib/SwapMessage:SwapMessageCodec:0xdb38EB076d423557F24dE1634358aa750Fd9A0DB --rpc-url $RPC_URL --private-key $PRIVATE_KEY --broadcast
-```
-
-Base
-```
-forge script script/DeployValueRouter.s.sol --libraries src/lib/Bytes.sol/Bytes:Bytes:0x064B37a5E001E73020166c32B06Eb07372659029 src/lib/CCTPMessage.sol/CCTPMessage:0x6ec82CDE6a64B8752017C0160E272f77BbBeD089 src/lib/SwapMessage:SwapMessageCodec:0xdb38EB076d423557F24dE1634358aa750Fd9A0DB --rpc-url $RPC_URL --private-key $PRIVATE_KEY --broadcast
-```
-
-Polygon
-```
-forge script script/DeployValueRouter.s.sol --libraries src/lib/Bytes.sol/Bytes:Bytes:0x064B37a5E001E73020166c32B06Eb07372659029 src/lib/CCTPMessage.sol/CCTPMessage:0x6ec82CDE6a64B8752017C0160E272f77BbBeD089 src/lib/SwapMessage:SwapMessageCodec:0x06bCcac1D96Ec89c1Dd62F715e0487b8c6B9FC92 --rpc-url $RPC_URL --private-key $PRIVATE_KEY --broadcast
-```
-
--- script/ConfigValueRouter.s.sol
+++ script/ConfigValueRouter.s.sol
@@ -14,7 +14,7 @@ contract ConfigValueRouter is Script {
             contractAddress = 0x0000000000000000000000000000000000000000;
         } else if (chainId == 43114) {
             // Avalanche-C
-            contractAddress = 0x0000000000000000000000000000000000000000;
+            contractAddress = 0x47faeB3d444b9361FdB8a04Ce6b4B496B1f17Da5;
         } else if (chainId == 10) {
             // Optimism
             contractAddress = 0x0000000000000000000000000000000000000000;
@@ -32,14 +32,14 @@ contract ConfigValueRouter is Script {
         }
         address admin = 0x840fC5Cf55019904f85F91DE5aD211248FfC5F4d;
 
-        bytes32 nobleCaller = 0x000000000000000000000000bbc905eb987498003c94d64bba25ee5efe84b51e;
+        bytes32 nobleCaller = bytes32(0x000000000000000000000000bbc905eb987498003c94d64bba25ee5efe84b51e);
 
-        bytes32 solanaValueRouter = 0x2e12357ca301f806dc09b9aae1a6dabc86954c1fd0593b6df8708b1bee83e888;
-        bytes32 solanaValueRouterCaller = 0x95472250619365db8d7f3d4a655da9383d3f66d0615442c55edfe4679cda4a0d;
-        bytes32 programUsdcAccount = 0x4ce9ba6d8ed6265fe5d9262abe2522c4970bbe42a00ad7e78a36422c932e5544;
-        bytes32 solanaCctpReceiver = 0x4352e98d0dfef2a95d0a81a56c960dec102111ac0ba732ab8858a5891dfb5df0;
+        bytes32 solanaValueRouter = bytes32(0xa6c0e7016507277d7503fea5e160d27919005380d75db7879e91c6f98cbf26db);
+        bytes32 solanaValueRouterCaller = bytes32(0xbcca4980b2dd4667e41caf3a4108d667356db8b852abd4dcd6db393847e47fd2);
+        bytes32 programUsdcAccount = bytes32(0x9a5199e0b220389ae61787e72e3adfba437dc2d8468d87bf402438c63fdb6f89);
+        bytes32 solanaCctpReceiver = bytes32(0x4352e98d0dfef2a95d0a81a56c960dec102111ac0ba732ab8858a5891dfb5df0);
 
-        uint32[] memory remoteDomains;
+        uint32[] memory remoteDomains = new uint32[](8);
         remoteDomains[0] = 0;
         remoteDomains[1] = 1;
         remoteDomains[2] = 2;
@@ -49,15 +49,15 @@ contract ConfigValueRouter is Script {
         remoteDomains[6] = 6;
         remoteDomains[7] = 7;
 
-        bytes32[] memory routers;
-        routers[0] = 0x000000000000000000000000000000000000000000000000000000000000abcd;
-        routers[1] = 0x000000000000000000000000000000000000000000000000000000000000abcd;
-        routers[2] = 0x000000000000000000000000000000000000000000000000000000000000abcd;
-        routers[3] = 0x000000000000000000000000000000000000000000000000000000000000abcd;
-        routers[4] = 0x000000000000000000000000000000000000000000000000000000000000abcd;
-        routers[5] = 0x2e12357ca301f806dc09b9aae1a6dabc86954c1fd0593b6df8708b1bee83e888;
-        routers[6] = 0x000000000000000000000000000000000000000000000000000000000000abcd;
-        routers[7] = 0x000000000000000000000000000000000000000000000000000000000000abcd;
+        bytes32[] memory routers = new bytes32[](8);
+        routers[0] = bytes32(0x000000000000000000000000000000000000000000000000000000000000abcd);
+        routers[1] = bytes32(0x000000000000000000000000000000000000000000000000000000000000abcd);
+        routers[2] = bytes32(0x000000000000000000000000000000000000000000000000000000000000abcd);
+        routers[3] = bytes32(0x000000000000000000000000000000000000000000000000000000000000abcd);
+        routers[4] = bytes32(0x000000000000000000000000000000000000000000000000000000000000abcd);
+        routers[5] = solanaValueRouter;
+        routers[6] = bytes32(0x000000000000000000000000000000000000000000000000000000000000abcd);
+        routers[7] = bytes32(0x000000000000000000000000000000000000000000000000000000000000abcd);
 
         vm.startBroadcast();
 
